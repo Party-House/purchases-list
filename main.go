aed4f4e2b06bf449a1c00bd08c6fb2cfaa7633aa
andrepestana92 <andre.pest92@gmail.com>
2018-04-16 00:14:47 -0300

    Getting all list from mongo db

--- main.go
+++ main.go
@@ -3,31 +3,51 @@ package main
 import (
     "fmt"
     "log"
-    "html"
     "net/http"
+    "encoding/json"
+    "os"
     "gopkg.in/mgo.v2"
     "gopkg.in/mgo.v2/bson"
     "github.com/gorilla/mux"
 )
 
+type Purchase struct {
+    ID bson.ObjectId `bson:"_id,omitempty" json:"id"`
+    Items []string  `bson:"Items" json:"items"`
+    Bought bool     `json:"bought"`
+}
+
+func setUpConnection() *mgo.Collection{
+    session, err := mgo.Dial(os.Getenv("MONGODB_URI"))
+    if err != nil {
+        panic(err)
+    }
+
+    c := session.DB(os.Getenv("MONGODB_DB")).C(os.Getenv("MONGODB_COLLECTION"))
+    return c
+}
+
 func main() {
 
     router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-    router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
+    router.HandleFunc("/", GetPurchaseList)
+    router.HandleFunc("/todos", PostPurchaceItem)
+    router.HandleFunc("/todos/{todoId}", UpdatePurchase)
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func GetPurchaseList(w http.ResponseWriter, r *http.Request) {
+    var result []Purchase
+    c := setUpConnection()
+    c.Find(bson.M{}).All(&result)
+    json.NewEncoder(w).Encode(result)
 }
 
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
+func PostPurchaceItem(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Todo Index!")
 }
 
-func TodoShow(w http.ResponseWriter, r *http.Request) {
+func UpdatePurchase(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     todoId := vars["todoId"]
     fmt.Fprintln(w, "Todo show:", todoId)
